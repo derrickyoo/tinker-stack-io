3bb8e62d67e03208bf1d31bb8a6a508c0f3dccc1
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-05 08:25:06 -0700

    Added base component and client index to fix #6

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+console.log("Data Layer Control (Redux)");
+/* eslint-disable import/first */
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import App from "./components/App";
+
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
