a5601a52d5b2f0459e75ceb91a473e9813c43eb1
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-05 14:56:53 -0700

    Added base routing using react-router-dom

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h1>Header</h1>;
+const Landing = () => <h1>Landing</h1>;
+const Dashboard = () => <h1>Dashboard</h1>;
+const Profile = () => <h1>Profile</h1>;
 
 const App = () => {
-  return <h1>Welcome to the TinkerStack.io client-side</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route path="/profile" component={Profile} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
