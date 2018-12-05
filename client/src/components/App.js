5e29b410c14b5ae6bfe9c30ae07cce4977293696
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-05 15:38:27 -0700

    Added new Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const Header = () => <h1>Header</h1>;
+import Header from "./Header";
+
 const Landing = () => <h1>Landing</h1>;
 const Dashboard = () => <h1>Dashboard</h1>;
 const Profile = () => <h1>Profile</h1>;
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <h2>Header</h2>;
+  }
+}
+
+export default Header;
