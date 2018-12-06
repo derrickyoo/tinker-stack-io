346f49bca59b595e3583b6c88cbf671529c5cb30
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-05 18:21:01 -0700

    Refactored App to class extending Component from react

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Header from "./Header";
@@ -7,19 +7,21 @@ const Landing = () => <h1>Landing</h1>;
 const Dashboard = () => <h1>Dashboard</h1>;
 const Profile = () => <h1>Profile</h1>;
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <BrowserRouter>
-        <div className="container">
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route path="/profile" component={Profile} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <BrowserRouter>
+          <div className="container">
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route path="/profile" component={Profile} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
