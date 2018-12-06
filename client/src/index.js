298e915efcafc31ed8b54197aa5fe1464382df08
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-05 18:16:18 -0700

    Created first action creator that uses redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-console.log("Data Layer Control (Redux)");
 /* eslint-disable import/first */
 // Non JavaScript files will need to have specific extension.
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,6 +6,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
@@ -14,7 +14,7 @@ import reducers from "./reducers";
 // All React application state is stored as a single object
 // Reducers specify HOW the application's state changes in response to actions
 // sent to the store.
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
