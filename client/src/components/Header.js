7e881d0f18327a5581cbf9cca8856a75ac1bb9b3
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-05 17:28:56 -0700

    Updated Header class attributes to className for react component syntax

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,12 +3,12 @@ import React, { Component } from "react";
 class Header extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="/">
           TinkerStack
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarSupportedContent"
@@ -16,24 +16,24 @@ class Header extends Component {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon" />
+          <span className="navbar-toggler-icon" />
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">
-                Home <span class="sr-only">(current)</span>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home <span className="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
                 Link
               </a>
             </li>
-            <li class="nav-item dropdown">
+            <li className="nav-item dropdown">
               <a
-                class="nav-link dropdown-toggle"
+                className="nav-link dropdown-toggle"
                 href="/"
                 id="navbarDropdown"
                 role="button"
@@ -43,32 +43,34 @@ class Header extends Component {
               >
                 Dropdown
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="/">
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="/">
                   Action
                 </a>
-                <a class="dropdown-item" href="/">
+                <a className="dropdown-item" href="/">
                   Another action
                 </a>
-                <div class="dropdown-divider" />
-                <a class="dropdown-item" href="/">
+                <div className="dropdown-divider" />
+                <a className="dropdown-item" href="/">
                   Something else here
                 </a>
               </div>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
+          <form className="form-inline my-2 my-lg-0">
             <input
-              class="form-control mr-sm-2"
+              className="form-control mr-sm-2"
               type="search"
               placeholder="Search"
               aria-label="Search"
             />
           </form>
-          <span class="navbar-text mr-sm-2">
+          <span className="navbar-text mr-sm-2">
             <a href="/">Log in</a> or
           </span>
-          <button class="btn btn-outline-success my-2 my-sm-0">Sign Up</button>
+          <button className="btn btn-outline-success my-2 my-sm-0">
+            Sign Up
+          </button>
         </div>
       </nav>
     );
