c0161168eb7f360f0275e9b1df3d2fa1e6a82961
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-08 13:26:28 -0700

    Created a base Survey model Schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-// Lines 3 and 4 are identical - destructuring
-// const Schema = mongoose.Schema;
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
