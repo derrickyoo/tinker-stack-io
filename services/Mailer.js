34cae8d54adbfaae7e2e5deeab1b30d608e98d9e
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-09 12:17:40 -0700

    Refactoring Mailer class with new sendgrid monorepo

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,9 +1,21 @@
 const sgMail = require("@sendgrid/mail");
+const helpers = require("@sendgrid/helpers").classes;
 const keys = require("../config/keys");
 
-class Mailer {
+class Mailer extends helpers.Mail {
   constructor({ subject, recipients }, content) {
     super();
+
+    this.from_email = new helpers.EmailAddress("no-reply@tinker-stack.io");
+    this.subject = subject;
+    this.body = this.addHtmlContent(content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helpers.EmailAddress(email);
+    });
   }
 }
 
