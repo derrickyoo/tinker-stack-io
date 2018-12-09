c7d115746f2d59e8d827ddcea8ffb3096e710499
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-09 09:57:09 -0700

    Revert Mailer to basic class

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,15 +1,9 @@
 const sgMail = require("@sendgrid/mail");
-const helper = sgMail.mail;
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mailer {
+class Mailer {
   constructor({ subject, recipients }, content) {
     super();
-
-    this.from_email = new helper.from_email("no-reply@tinkerstack.io");
-    this.subject = subject;
-    this.body = new helper.content("text/html", content);
-    this.recipients = this.formatAddresses(recipients);
   }
 }
 
