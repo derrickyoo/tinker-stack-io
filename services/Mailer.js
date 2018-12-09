f6a9ce83e2164c0e904d90ebcae25a20ead105c9
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-09 09:22:11 -0700

    Creating Mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,15 @@ const sgMail = require("@sendgrid/mail");
 const helper = sgMail.mail;
 const keys = require("../config/keys");
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mailer {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.from_email("no-reply@tinkerstack.io");
+    this.subject = subject;
+    this.body = new helper.content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
 
 module.exports = Mailer;
