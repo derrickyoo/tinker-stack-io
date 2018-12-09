30a896eda46595ba4a7bb7961385fd79ce0fefe4
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-08 17:28:54 -0700

    Added sendGridKey

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,12 @@ const dotenv = require("dotenv");
 dotenv.load();
 
 module.exports = {
-  nodeEnv: process.env.NODE_ENV
+  nodeEnv: process.env.NODE_ENV,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripPublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 };
