5dad20c0ac3580e1b6084c985182a83bdde9d502
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-10 09:00:09 -0700

    Refactored Mailer to only use sendgrid/mail

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -20,6 +20,7 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
+
     mailer.send();
   });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,43 +1,29 @@
-const client = require("@sendgrid/client");
-const helpers = require("@sendgrid/helpers").classes;
+const sgMail = require("@sendgrid/mail");
 const keys = require("../config/keys");
 
-class Mailer extends helpers.Mail {
+class Mailer {
   constructor({ subject, recipients }, content) {
     super();
 
-    this.sgClient = client.setApiKey(keys.sendGridKey);
-    this.from_email = new helpers.EmailAddress("no-reply@tinker-stack.io");
-    this.subject = subject;
-    this.body = this.addHtmlContent(content);
-    this.recipients = this.formatAddresses(recipients);
-
-    this.addClickTracking();
-    this.setPersonalizations(this.recipients);
-  }
-
-  formatAddresses(recipients) {
-    return recipients.map(({ email }) => {
-      return new helpers.EmailAddress(email);
-    });
-  }
-
-  addClickTracking() {
-    const trackingSettings = {
-      clickTracking: { enable: true, enableText: true }
+    this.message = {
+      from: "no-reply@tinker-stack.io",
+      subject: subject,
+      html: content,
+      to: recipients,
+      trackingSettings: {
+        clickTracking: { enable: true, enableText: true }
+      }
     };
-    this.setTrackingSettings(trackingSettings);
+    sgMail.setApiKey(keys.sendGridKey);
   }
 
   async send() {
-    const request = {
-      method: "POST",
-      url: "/v3/mail/send",
-      body: this.toJSON()
-    };
-
-    const response = await this.sgClient.request(request);
-    return response;
+    try {
+      response = await sgMail.sendMultiple(this.message);
+      return response;
+    } catch (err) {
+      throw err;
+    }
   }
 }
 
