c5870b5ce4b4f629407f254f70740bf5451b3bb9
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-13 15:55:24 -0700

    Added SurveyFormReview back button

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,14 @@
+// SurveFormReview shows users their form inputs for review
 import React from "react";
 
-const SurveyFormReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
   return (
     <div>
       <h4>Please Confirm Your Entries!</h4>
+
+      <button type="button" className="btn btn-warning" onClick={onCancel}>
+        Back
+      </button>
     </div>
   );
 };
