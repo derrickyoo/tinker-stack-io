df3d28637058084520012b81cbd51c0a31e016d7
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-23 15:12:55 -0700

    Created surveyList component and imported into Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import SurveyList from "./surveys/SurveyList";
+
 const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
+      <SurveyList />
       <Link
         to="/surveys/new"
         className="btn btn-primary btn-lg active"
