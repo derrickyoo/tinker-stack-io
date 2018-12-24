eba51631a47d234e388b5abf2c241007412f924e
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-23 19:49:01 -0700

    Rendered content to dashboard

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,8 +7,29 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
 
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card mb-2" key={survey._id}>
+          <div className="card-body">
+            <span>{survey.title}</span>
+            <p>{survey.body}</p>
+            <p>Sent On: {new Date(survey.dateSent).toLocaleDateString()}</p>
+
+            <a href="/" className="card-link">
+              Yes: {survey.yes}
+            </a>
+            <a href="/" className="card-link">
+              No: {survey.no}
+            </a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
