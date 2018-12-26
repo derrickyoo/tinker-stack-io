8948943b80b9ebf19e2ef31297aba9f8150efac6
Derrick Yoo <derrickkyoo@gmail.com>
2018-12-26 09:42:37 -0700

    Adding to User model for email and pass auth

--- models/User.js
+++ models/User.js
@@ -1,10 +1,39 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   googleID: String,
-  credits: { type: Number, default: 0 }
+  credits: { type: Number, default: 0 },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: `${VALUE} is not a valid email!`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 mongoose.model("users", userSchema);
